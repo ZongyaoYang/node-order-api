57c4faf7d582d4dcf0f86b13d0ff765e97ae88c5
ZongyaoYang <zongyaoyang@gmail.com>
2026-08-27 16:14:48 -0600

    add the get method in controller

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -3,12 +3,30 @@ import { orderService } from "../services/orderService.js";
 function createOrder(req, res) {
   const createdOrder = orderService.createOrder(req.body);
 
-  res.status(201).location(`/api/orders/${createdOrder.orderId}`)
-  .json({
+  res.status(201).location(`/api/orders/${createdOrder.orderId}`).json({
     data: createdOrder,
   });
 }
 
-export const orderController = {
-    createOrder,
+function getOrderById(req, res) {
+  const { orderId } = req.params;
+
+  const order = orderService.getOrderById(orderId);
+
+  if (!order) {
+    return res.status(404).json({
+      error: {
+        code: "ORDER_NOT_FOUND",
+      },
+    });
+  }
+
+  return res.status(200).json({
+    data: order,
+  });
 }
+
+export const orderController = {
+  createOrder,
+  getOrderById,
+};
