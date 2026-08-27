2a33a2189a187e53a282c6b905cffc05143f6a3b
ZongyaoYang <zongyaoyang@gmail.com>
2026-08-27 11:54:49 -0600

    add order schema

---
+++ src/schemas/orderSchema.js
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const createOrderSchema = z.object(
+    {
+        memberId: z.string().trim().min(1, "Member ID is required."),
+        cardType: z.enum(
+            ["new", "replacement", "renewal"],
+            {
+                error: "Card type must be new, replacement, or renewal",
+            }
+        ),
+
+        shippingMethod: z.enum(
+            ["standard", "expedited"],
+            {
+                error: "Shipping method must be standard or expedited."
+            }
+        ),
+    }
+).strict();
